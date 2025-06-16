45479686494521b8c2fc166d797ca81e7ef28df4
Lamont Crook <lamont@adobe.com>
2025-06-16 15:35:39 -0600

    update teaser

--- src/components/teaser/teaser.js
+++ src/components/teaser/teaser.js
@@ -11,7 +11,7 @@ const Teaser = ({ content }) => {
 
   return (
     <React.Fragment>
-      <section className={'teaser ' + content.style} itemID={`urn:aemconnection:${content._path}/jcr:content/data/master`} itemfilter='cf' itemType='reference' itemScope>
+      <section className={'teaser ' + content.style}>
         <div className='container'>
           {content.asset.__typename === 'MultimediaRef' &&
             (<Video content={content.asset} />)}
@@ -21,25 +21,25 @@ const Teaser = ({ content }) => {
 
           <div className='content-block'>
             {content.title && content.style === 'hero' && (
-              <h1 itemProp='title' itemType='text'>{content.title}</h1>
+              <h1>{content.title}</h1>
             )}
 
             {content.title && content.style === 'featured' && (
-              <h2 itemProp='title' itemType='text'>{content.title}</h2>
+              <h2>{content.title}</h2>
             )}
 
             <span className='seperator'></span>
 
             {content.preTitle && content.style === 'hero' && (
-              <h2 itemProp='preTitle' itemType='text'>{content.preTitle}</h2>
+              <h2>{content.preTitle}</h2>
             )}
 
             {content.preTitle && content.style === 'featured' && (
-              <h5 itemProp='preTitle' itemType='text'>{content.preTitle}</h5>
+              <h5>{content.preTitle}</h5>
             )}
 
             {content.description && (
-              <span data-aue-prop='description' data-aue-type='richtext' data-aue-label='Description'>{mapJsonRichText(content.description.json)}</span>
+              <span>{mapJsonRichText(content.description.json)}</span>
             )}
 
           </div>
