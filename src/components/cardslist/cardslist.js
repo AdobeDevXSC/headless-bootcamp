e1cc52fdf648ecea1ffb53dad5cc98c46f0f5eea
Lamont Crook <lamont@adobe.com>
2025-06-16 18:42:40 -0600

    Update cardslist.js

--- src/components/cardslist/cardslist.js
+++ src/components/cardslist/cardslist.js
@@ -6,21 +6,22 @@ import { mapJsonRichText } from '../../utils/renderRichText';
 import './cardslist.css';
 
 const CardsList = ({ content }) => {
-  const editorProps = (item) => {
-    return {
-      'data-aue-resource': `urn:aemconnection:${item?._path}/jcr:content/data/${item?._variation}`,
-      'data-aue-type': 'container',
-      'data-aue-label': 'Cards',
-      'data-aue-behavior': 'component',
-      'data-aue-model': item?._model?._path,
-      'data-aue-prop': 'card'
-    }
+   const editorProps = {
+    'data-aue-resource': `urn:aemconnection:${content._path}/jcr:content/data/${content._variation}`,
+    'data-aue-type': 'container',
+    'data-aue-label': content?.headline?.plaintext,
+    'data-aue-behavior': 'component',
+    'data-aue-model': content?._model?._path,
+    'data-aue-filter': 'image-list',
+    'data-aue-prop': 'card'
   };
 
   return (
     <React.Fragment>
-      {content && content.headline && content.headline.html && <span className='headline' dangerouslySetInnerHTML={{ __html: content.headline.html }} />}
-      <div className='cards' {...editorProps(content)}>
+      {content && content.headline && content.headline.html && 
+        <span className='headline' dangerouslySetInnerHTML={{ __html: content.headline.html }} />
+      }
+      <div className='cards' {...editorProps}>
         {content && content.card && content.card.map((item) => (
           <Card key={item._path} content={item} />
         ))}
@@ -30,26 +31,29 @@ const CardsList = ({ content }) => {
 };
 
 const Card = ({ content }) => {
+
   const editorProps = {
+    'data-aue-resource': `urn:aemconnection:${content._path}/jcr:content/data/${content._variation}`,
     'data-aue-type': 'reference',
-    'data-aue-behavior': 'component',
+    'data-aue-label': content._metadata ? parseName(content) : 'Card',
+    'data-aue-model': content?._model?._path,
+    'data-aue-behavior': 'component'
   };
 
   return (
     <div className='card' key={content._path} {...editorProps}>
       {content && content.primaryImage && <Image asset={content.primaryImage} />}
-      <h3>{content._metadata && parseName(content)}</h3>
-      <div>{mapJsonRichText(content.description.json)}</div>
+      <h3 data-aue-prop='title' data-aue-type='text' data-aue-label='Title'>{content.title}</h3>
+      <div data-aue-prop='description' data-aue-type='text' data-aue-label='Description'>{mapJsonRichText(content.description.json)}</div>
     </div>
   );
 };
 
 Card.propTypes = {
-  content: PropTypes.object
+  content: PropTypes.array
 };
 
 const parseName = ({ _metadata }) => {
-  if (!_metadata) return;
   let cardName = '';
   _metadata.stringMetadata.map((item) => {
     if (item.name === 'title') {
