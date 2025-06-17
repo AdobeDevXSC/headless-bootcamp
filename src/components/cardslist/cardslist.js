edc66bdfa73ba6ba900e7babec4a78f4b3c6dc27
Lamont Crook <lamont@adobe.com>
2025-06-17 07:41:58 -0600

    Update cardslist.js

--- src/components/cardslist/cardslist.js
+++ src/components/cardslist/cardslist.js
@@ -5,33 +5,31 @@ import { mapJsonRichText } from '../../utils/renderRichText';
 
 import './cardslist.css';
 
-const ImageList = ({ content }) => {
+const CardsList = ({ content }) => {
 
   return (
-    <div className='imagelist'>
-      {content.items && content.items.map((items) => (
-        <Cards key={items} content={items.listItems} />
-      ))}
+    <React.Fragment>
+      {content && content.headline && content.headline.html && <span className='headline' dangerouslySetInnerHTML={{ __html: content.headline.html }} />}
+      <div className='cards'>
+        {content && content.card && content.card.map((item) => (
+          <Card key={item._path} content={item} />
+        ))}
+      </div>
+    </React.Fragment >
+  );
+};
 
+const Card = ({ content }) => {
+  return (
+    <div className='card' key={content._path}>
+      {content && content.primaryImage && <Image asset={content.primaryImage} />}
+      <h3>{content._metadata && parseName(content)}</h3>
+      <div>{mapJsonRichText(content.description.json)}</div>
     </div>
   );
 };
 
-const Cards = ({ content }) => {
-  return (
-    <div className='cards'>
-      {content && content.map((card) => (
-        <div className='card' key={card._path}>
-          <Image src={card.asset._dynamicUrl} />
-          <h3>{card._metadata && parseName(card)}</h3>
-          <div>{mapJsonRichText(card.description.json)}</div>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-Cards.propTypes = {
+Card.propTypes = {
   content: PropTypes.array
 };
 
@@ -45,8 +43,8 @@ const parseName = ({ _metadata }) => {
   return cardName;
 };
 
-ImageList.propTypes = {
+CardsList.propTypes = {
   content: PropTypes.object
 };
 
-export default ImageList;
+export default CardsList;
